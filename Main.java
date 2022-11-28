9c51e9cc981f26d79a3838d344725c0cf5d8a9b6
AmberOzenogu <amberozenogu@gmail.com>
2022-11-28 18:22:05 +0100

    FirstJavaProject - Task 14

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World, this is my first Java project.\nBeing added to my TestCodeCamp git repository.");
+        System.out.println("Today I learned about the advantages of Java, some of which include:");
+        System.out.println("1. Java is simple to learn.");
+        System.out.println("2. Java is platform-independent, meaning it can be written once and ran anywhere.");
+        System.out.println("3. Java is object-oriented, which makes it more practical.");
+    }
+}
